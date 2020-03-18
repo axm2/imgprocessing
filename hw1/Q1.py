37687488a078364075d0164fc993564b15583846
Omar Mirza <axmirza2@yahoo.com>
2020-03-18 02:17:25 -0400

    histograms now display as subplots

--- hw1/Q1.py
+++ hw1/Q1.py
@@ -4,23 +4,29 @@ import matplotlib.pyplot as plt
 from collections import OrderedDict
 from collections import Counter
 
-def histogram_maker(channel, title):
+def histogram_maker(channel, title, ax=None):
+    ax = ax or plt.gca()
     E = Counter(channel.flatten()) # working as intended
     D = OrderedDict(sorted(E.items()))
-    plt.bar(range(len(D)), list(D.values()), align='center')
-    plt.xlim(right=255)
-    plt.title(title)
-    plt.xlabel('pixel intensity')
-    plt.ylabel('number of pixels')
-    plt.show()
+    line = ax.bar(range(len(D)), list(D.values()), align='center')
+    ax.set_xlim(right=255)
+    ax.set_title(title)
+    ax.set_xlabel('pixel intensity')
+    ax.set_ylabel('number of pixels')
+    return line
 
 #q1 use opencv, numpy, matplot
+fig, ((ax1, ax2, ax3), (bx1, bx2, bx3)) = plt.subplots(2, 3, sharex='all', sharey='all')
 overexposed_img = cv2.imread('E:/Documents/imgprocessing/hw1/overexposed.jpg')
 underexposed_img = cv2.imread('E:/Documents/imgprocessing/hw1/underexposed.jpg')
 img = cv2.imread('E:/Documents/bighead.jpg')
 red_channel = img[:,:,2]
 green_channel = img[:,:,1]
 blue_channel = img[:,:,0]
-histogram_maker(red_channel, 'red channel histogram')
-histogram_maker(green_channel, 'green channel histogram')
-histogram_maker(blue_channel, 'blue channel histogram')
+histogram_maker(red_channel, 'red channel histogram', ax1)
+histogram_maker(green_channel, 'green channel histogram', ax2)
+histogram_maker(blue_channel, 'blue channel histogram', ax3)
+histogram_maker(red_channel, 'red channel histogram', bx1)
+histogram_maker(green_channel, 'green channel histogram', bx2)
+histogram_maker(blue_channel, 'blue channel histogram', bx3)
+plt.show()
