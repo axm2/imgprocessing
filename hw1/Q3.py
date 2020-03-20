89ebc5090b539e418e93f2e1959947174847cb46
psingh875 <49342348+psingh875@users.noreply.github.com>
2020-03-20 19:05:02 -0400

    Add files via upload

---
+++ hw1/Q3.py
@@ -0,0 +1,24 @@
+
+from spectral import *
+
+import matplotlib
+matplotlib.use('TkAgg')
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+img = open_image('TIPJUL1.lan')
+img.__class__
+rgb_image = np.zeros((img.shape[0],img.shape[1],3),np.int32)
+rgb_image[:,:,0] = np.reshape(img[:,:,3],(169,169))
+rgb_image[:,:,1] = np.reshape(img[:,:,2],(169,169))
+rgb_image[:,:,2] = np.reshape(img[:,:,1],(169,169))
+plt.figure(1)
+plt.imshow(rgb_image)
+plt.title("False Color Image")
+ndvi_image = (img[:,:,3]-img[:,:,2])/(img[:,:,3]+img[:,:,2])
+plt.figure(2)
+plt.imshow(ndvi_image.reshape(169,169), cmap=plt.cm.gray)
+plt.title("NDVI Image")
+plt.show()
+print()
