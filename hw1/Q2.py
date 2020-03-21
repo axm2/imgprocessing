df0bde8532f02175e98a91950e6c9e0af8ae1ef1
Adam Binder <48499553+AdamMBinder@users.noreply.github.com>
2020-03-21 17:13:26 -0400

    Updated Q2 Paths

--- hw1/Q2.py
+++ hw1/Q2.py
@@ -19,7 +19,7 @@ img = cv2.cvtColor(img3, cv2.COLOR_BGR2GRAY)
 ret, thresh1 = cv2.threshold(img, 120, 255, cv2.THRESH_BINARY)
 
 # Save output img
-cv2.imwrite('output/Q2_binary_threshold.jpg', img)
+cv2.imwrite('output/Q2_binary_threshold.png', img)
 
 histr = cv2.calcHist([img], [0], None, [256], [0, 256])
 
